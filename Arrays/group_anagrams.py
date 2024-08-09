761ddad1d752c53c8f101e4ce28725a190ec72f2
Malcome Baggett <MalcomeBaggett@gmail.com>
2024-08-08 20:25:14 -0400

    added group_anagrams

---
+++ Arrays/group_anagrams.py
@@ -0,0 +1,13 @@
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+
+def group_anagrams(strs):
+    ans = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        ans[tuple(count)].append(s)
+    return ans.values()
